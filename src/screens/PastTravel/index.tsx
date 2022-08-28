bd4941c8fe7d95ceecfef2afe01c03a87a1add44
Joao Fernandes <joao.fernandes@impar.com.br>
2022-08-28 00:33:08 -0300

    styled past travel screen

--- src/screens/PastTravel/index.tsx
+++ src/screens/PastTravel/index.tsx
@@ -1,6 +1,6 @@
 // External libraries
 import React from 'react'
-import { Alert, PermissionsAndroid, Platform } from 'react-native'
+import { Alert, PermissionsAndroid, Platform, View } from 'react-native'
 import MapView, { Marker } from 'react-native-maps'
 import MapViewDirections from 'react-native-maps-directions'
 import { useTheme } from 'styled-components/native'
@@ -12,47 +12,67 @@ import Header from '../../components/Header'
 import { propsPastTravel } from '../../routes/models'
 
 // Styled
-import styles, { Container } from './styles'
+import styles, {
+  Container,
+  Content,
+  MapViewContent,
+  PastTravelStatus,
+  PastTravelText,
+} from './styles'
 
 const PastTravel: React.FC<propsPastTravel> = ({ route }) => {
   const { colors } = useTheme()
-  const origin = route.params.pastTravelItem.origin
-  const destination = route.params.pastTravelItem.destination
+  const pastTravelItem = route.params.pastTravelItem
+  const origin = pastTravelItem.origin
+  const destination = pastTravelItem.destination
 
   return (
     <Container>
       <Header title={'Past Travel #' + route.params.pastTravelItem.id} />
-      <MapView
-        style={styles.mapView}
-        onMapReady={() => {
-          Platform.OS === 'android' &&
-            PermissionsAndroid.request(
-              PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-            )
-        }}
-        initialRegion={{
-          latitude: origin.latitude,
-          longitude: origin.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        loadingEnabled
-        showsUserLocation
-        zoomEnabled>
-        <>
-          <Marker coordinate={origin} pinColor={colors.danger} />
-          <Marker coordinate={destination} pinColor={colors.danger} />
-          <MapViewDirections
-            origin={origin}
-            destination={destination}
-            apikey={'AIzaSyCFpO6MAYw5GIErpFdXymewwe1RfB79Zxg'}
-          />
-          {Alert.alert(
-            'OOPS!',
-            'The "Travel" function is not activated as payment is required and this app has training functionality only.',
-          )}
-        </>
-      </MapView>
+      <Content>
+        <MapViewContent>
+          <MapView
+            style={styles.mapView}
+            onMapReady={() => {
+              Platform.OS === 'android' &&
+                PermissionsAndroid.request(
+                  PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                )
+            }}
+            initialRegion={{
+              latitude: origin.latitude,
+              longitude: origin.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            loadingEnabled
+            zoomEnabled>
+            <>
+              <Marker coordinate={origin} pinColor={colors.danger} />
+              <Marker coordinate={destination} pinColor={colors.danger} />
+              <MapViewDirections
+                origin={origin}
+                destination={destination}
+                apikey={'AIzaSyCFpO6MAYw5GIErpFdXymewwe1RfB79Zxg'}
+              />
+              {Alert.alert(
+                'OOPS!',
+                'The "Travel" function is not activated as payment is required and this app has training functionality only.',
+              )}
+            </>
+          </MapView>
+        </MapViewContent>
+        <View>
+          <PastTravelStatus>
+            <PastTravelText>Price:</PastTravelText>
+            <PastTravelText>${pastTravelItem.price}</PastTravelText>
+          </PastTravelStatus>
+          <PastTravelStatus>
+            <PastTravelText>Time travel:</PastTravelText>
+            <PastTravelText>${pastTravelItem.time}</PastTravelText>
+          </PastTravelStatus>
+        </View>
+      </Content>
     </Container>
   )
 }
--- src/screens/PastTravel/styles.ts
+++ src/screens/PastTravel/styles.ts
@@ -8,12 +8,53 @@ export const Container = styled.SafeAreaView`
   background-color: ${props => props.theme.colors.secondary};
 `
 
+export const Content = styled.View`
+  flex: 1;
+
+  justify-content: space-between;
+`
+
+export const MapViewContent = styled.View`
+  width: 100%;
+
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${props => props.theme.colors.secondary};
+`
+
+export const PastTravelStatus = styled.View`
+  height: 36px;
+
+  margin-bottom: 15px;
+  margin-right: 18px;
+  margin-left: 18px;
+
+  align-items: flex-end;
+  justify-content: space-between;
+  flex-direction: row;
+
+  border-bottom-width: 2px;
+  border-bottom-style: solid;
+  border-bottom-color: ${props => props.theme.colors.quaternary};
+`
+
+export const PastTravelText = styled.Text`
+  margin-bottom: 4px;
+
+  color: ${props => props.theme.colors.soft};
+  font-family: ${props => props.theme.fonts.h4};
+  font-size: 18px;
+`
+
 const { width, height } = Dimensions.get('screen')
 
 const styles = StyleSheet.create({
   mapView: {
-    width: width,
-    height: height - 80,
+    width: width - 36,
+    height: height - 240,
+    marginTop: 18,
+    marginBottom: 32,
   },
 })
 
