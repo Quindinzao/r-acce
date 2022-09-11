ebca5893beb15ca0733b048d14b96d23f61b8378
Joao Fernandes <joao.fernandes@impar.com.br>
2022-09-11 16:16:47 -0300

    add components to settings screen

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,12 +1,75 @@
 // External libraries
-import { useNavigation } from '@react-navigation/native'
-import React from 'react'
+import React, { useContext } from 'react'
+import { View } from 'react-native'
+import { useTheme } from 'styled-components/native'
+
+// Components
+import Header from '../../components/Header'
+import Touchable from '../../components/Touchable'
+
+// Contexts
+import { AuthContext, useAuth } from '../../contexts/Auth'
+
+// Services
+import data from '../../services/data.json'
+
+// Assets
+import LogOut from '../../assets/images/logout.svg'
 
 // Styled
-import { Container } from './styles'
+import { Container, ContentSettings, RowSettings, TextSetting } from './styles'
 
 const Settings: React.FC = () => {
-  return <Container />
+  const { signOut } = useAuth()
+  const { colors } = useTheme()
+  const dataContext = useContext(AuthContext)
+  const dataUser = data.users.find(value => {
+    if (value.email === dataContext.authData?.email) {
+      return value
+    }
+  })
+  const formattedCardNumber = dataUser?.card_number.replace(
+    /(\d{4})(\d{4})(\d{4})(\d{4})/,
+    '$1.$2.$3.$4',
+  )
+  return (
+    <Container>
+      <Header title="Settings" />
+      <ContentSettings>
+        <View>
+          <RowSettings>
+            <TextSetting>Name:</TextSetting>
+            <TextSetting>{dataUser?.username}</TextSetting>
+          </RowSettings>
+          <RowSettings>
+            <TextSetting>E-mail:</TextSetting>
+            <TextSetting>{dataUser?.email}</TextSetting>
+          </RowSettings>
+          <RowSettings>
+            <TextSetting>Card number:</TextSetting>
+            <TextSetting>{formattedCardNumber}</TextSetting>
+          </RowSettings>
+          <RowSettings>
+            <TextSetting>Card security number:</TextSetting>
+            <TextSetting>{dataUser?.card_security_code}</TextSetting>
+          </RowSettings>
+          <RowSettings>
+            <TextSetting>Card expiration date:</TextSetting>
+            <TextSetting>{dataUser?.card_expiration_date}</TextSetting>
+          </RowSettings>
+        </View>
+        <Touchable
+          title="Sign out"
+          touchableType="filled"
+          backgroundColor={colors.primary}
+          color={colors.secondary}
+          margin="8px 0px 0px 0px"
+          onPress={() => signOut()}
+          image={<LogOut rotation={180} />}
+        />
+      </ContentSettings>
+    </Container>
+  )
 }
 
 export default Settings
--- src/screens/Settings/styles.ts
+++ src/screens/Settings/styles.ts
@@ -1,8 +1,34 @@
 // External libraries
 import styled from 'styled-components/native'
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
+  background-color: ${props => props.theme.colors.secondary};
+`
+
+export const ContentSettings = styled.View`
+  flex: 1;
+  margin-top: 12px;
+  padding: 18px;
+
+  justify-content: space-between;
+`
+
+export const RowSettings = styled.View`
+  margin-bottom: 24px;
+  padding-bottom: 8px;
+
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: #d6c2b4;
+
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+  flex-direction: row;
+`
+
+export const TextSetting = styled.Text`
+  color: ${props => props.theme.colors.primary};
+  font-size: 17px;
+  font-family: ${props => props.theme.fonts.h4};
 `
